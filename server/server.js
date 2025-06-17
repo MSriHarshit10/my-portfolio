69f59263c6b0dab581329272e62eb990651166b0
Harshit Mandalika <mandalika599@gmail.com>
2025-06-17 10:41:37 +0530

    Fix: Serve index.html from root route

--- server/server.js
+++ server/server.js
@@ -10,10 +10,19 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "../client")));
 
+// Serve index.html on root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
+
 // Routes
 app.use("/contact", contactRoutes);
 
 // Start server
+app.use((req, res) => {
+  res.status(404).send("404 - Page not found");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
